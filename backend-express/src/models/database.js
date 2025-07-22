b719093269c8045fa7360be6c285574bc38f011c
daffabs <daffabagus1933@gmail.com>
2025-07-22 23:44:17 +0700

    railway part 2

--- backend-express/src/models/database.js
+++ backend-express/src/models/database.js
@@ -1,21 +1,16 @@
-const mysql = require('mysql2');
-require('dotenv').config(); // Wajib agar .env terbaca
+const mysql = require('mysql2/promise');
+require('dotenv').config(); // Untuk jaga-jaga kalau pindah ke .env
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "yamabiko.proxy.rlwy.net",
   port: 38611,
   user: "root",
   password: "nJBDhFWkUFXXSWLtvbrXsetGOHLtdMDQ",
-  database: "railway"
+  database: "railway",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Gagal konek ke database:", err.message);
-  } else {
-    console.log("✅ Berhasil konek ke database Railway");
-  }
-});
 
 module.exports = db;
